9531121b8d50fc0abe67c354ae8719c9cac221ea
sandeeppandey0 <110024673+sandeeppandey0@users.noreply.github.com>
2023-08-01 05:45:08 +0000

    Added first html templates

--- app.py
+++ app.py
@@ -1,1 +1,9 @@
-print('Hello World!')
+from flask import Flask, render_template
+app = Flask(__name__)
+
+@app.route('/')
+def hello_world():
+    return render_template('Home.html')
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', debug=True)
